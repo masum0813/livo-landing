a4e84bad9bf7a95a17ee8baaeb1c7a2638567b43
masum0813 <murat_asu@hotmail.com>
2026-04-02 14:51:14 +0300

    Remove Turkish localization files and related configurations from Docusaurus

--- docusaurus/.docusaurus-en/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Volumes/Disk/Works/iOS/livo-landing/docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/Volumes/Disk/Works/iOS/livo-landing/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Volumes/Disk/Works/iOS/livo-landing/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Volumes/Disk/Works/iOS/livo-landing/docusaurus/src/css/custom.css"),
-];
